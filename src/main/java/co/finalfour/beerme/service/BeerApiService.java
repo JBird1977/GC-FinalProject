ac79fdd1e1179bed6ca7748e852e41ff2bb31722
katie <kateann58@gmail.com>
2019-03-07 17:08:57 -0500

    serve me beer

--- src/main/java/co/finalfour/beerme/service/BeerApiService.java
+++ src/main/java/co/finalfour/beerme/service/BeerApiService.java
@@ -1,5 +1,35 @@
 package co.finalfour.beerme.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import co.finalfour.beerme.entity.Brewery;
+import co.finalfour.beerme.entity.BreweryResponse;
+
+@Component
 public class BeerApiService {
 
+	@Value("${beer.key}")
+	private String key;
+
+	private RestTemplate restTemplate = new RestTemplate();
+	
+	public List<Brewery> findBreweries(String search) {
+
+		String url = UriComponentsBuilder.fromHttpUrl("http://api.brewerydb.com/v2/breweries/")
+				.queryParam("key", key)
+				.toUriString();
+		
+		System.out.println("FIND BREWERIES SERVICE = " + url);
+		
+		BreweryResponse response = restTemplate.getForObject(url, BreweryResponse.class);
+		
+		return response.getData();
+
+	}
+	
 }
